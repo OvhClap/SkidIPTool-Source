d0cac383d32cfcc08b752a4c71db9e6fd18f336c
Aeron ~~~~~ <lolskid1337@outlook.com>
2020-11-07 21:06:18 +0000

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Skid_Lookup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.IPAddress = new MetroFramework.Controls.MetroTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.outputbox = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.metroButton4 = new MetroFramework.Controls.MetroButton();
+            this.metroButton5 = new MetroFramework.Controls.MetroButton();
+            this.metroButton6 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // IPAddress
+            // 
+            this.IPAddress.Location = new System.Drawing.Point(12, 100);
+            this.IPAddress.Name = "IPAddress";
+            this.IPAddress.Size = new System.Drawing.Size(121, 23);
+            this.IPAddress.TabIndex = 0;
+            this.IPAddress.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(9, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP Address:";
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(12, 190);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(121, 29);
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "Geo Locate";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(222, 298);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(102, 23);
+            this.metroButton2.TabIndex = 3;
+            this.metroButton2.Text = "Clear";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(346, 298);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(102, 23);
+            this.metroButton3.TabIndex = 4;
+            this.metroButton3.Text = "Save";
+            this.metroButton3.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // outputbox
+            // 
+            this.outputbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.outputbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.outputbox.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.outputbox.Location = new System.Drawing.Point(222, 50);
+            this.outputbox.Name = "outputbox";
+            this.outputbox.Size = new System.Drawing.Size(226, 231);
+            this.outputbox.TabIndex = 5;
+            this.outputbox.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(7, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Skid Ip Tool";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(-1, 389);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Made by qk#0001";
+            // 
+            // metroButton4
+            // 
+            this.metroButton4.Location = new System.Drawing.Point(12, 225);
+            this.metroButton4.Name = "metroButton4";
+            this.metroButton4.Size = new System.Drawing.Size(121, 29);
+            this.metroButton4.TabIndex = 8;
+            this.metroButton4.Text = "Port Scan";
+            this.metroButton4.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton4.Click += new System.EventHandler(this.metroButton4_Click);
+            // 
+            // metroButton5
+            // 
+            this.metroButton5.Location = new System.Drawing.Point(12, 155);
+            this.metroButton5.Name = "metroButton5";
+            this.metroButton5.Size = new System.Drawing.Size(121, 29);
+            this.metroButton5.TabIndex = 9;
+            this.metroButton5.Text = "Ping";
+            this.metroButton5.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton5.Click += new System.EventHandler(this.metroButton5_Click);
+            // 
+            // metroButton6
+            // 
+            this.metroButton6.Location = new System.Drawing.Point(12, 260);
+            this.metroButton6.Name = "metroButton6";
+            this.metroButton6.Size = new System.Drawing.Size(121, 29);
+            this.metroButton6.TabIndex = 10;
+            this.metroButton6.Text = "Reverse Ip";
+            this.metroButton6.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton6.Click += new System.EventHandler(this.metroButton6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 403);
+            this.Controls.Add(this.metroButton6);
+            this.Controls.Add(this.metroButton5);
+            this.Controls.Add(this.metroButton4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.outputbox);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IPAddress);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Style = MetroFramework.MetroColorStyle.Black;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox IPAddress;
+        private System.Windows.Forms.Label label1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private System.Windows.Forms.RichTextBox outputbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private MetroFramework.Controls.MetroButton metroButton4;
+        private MetroFramework.Controls.MetroButton metroButton5;
+        private MetroFramework.Controls.MetroButton metroButton6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using DiscordRPC;
+using DiscordRPC.Logging;
+
+
+namespace Skid_Lookup
+{ 
+    public partial class Form1 : MetroForm
+    {
+        private WebClient wb;
+        public DiscordRpcClient client;
+        bool initalized = false;
+
+        public Form1()
+        {
+            this.wb = new WebClient();
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            initalized = true;
+            client = new DiscordRpcClient("765685233176805386");
+            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+            client.Initialize();
+
+            if (initalized == false)
+            {
+                MessageBox.Show("You need to initialize app first!");
+            }
+            else
+            {
+                client.SetPresence(new DiscordRPC.RichPresence()
+                {
+                    Details = "GeoLocating a skid",
+                    State = "www.skidforum.xyz",
+                    Timestamps = Timestamps.Now,
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = "main",
+                        LargeImageText = "www.skidforum.xyz",
+                        SmallImageText = "https://discord.gg/4nuq8fx",
+                        SmallImageKey = "main"
+                    }
+                }); ;
+            }
+            outputbox.Text = this.wb.DownloadString(string.Format("http://ip-api.com/line/" + this.IPAddress.Text));
+
+        }
+
+        private void ToolsBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            initalized = true;
+            client = new DiscordRpcClient("765685233176805386");
+            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+            client.Initialize();
+
+            if (initalized == false)
+            {
+                MessageBox.Show("You need to initialize app first!");
+            }
+            else
+            {
+                client.SetPresence(new DiscordRPC.RichPresence()
+                {
+                    Details = "Best Forum 2020",
+                    State = "www.skidforum.xyz",
+                    Timestamps = Timestamps.Now,
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = "main",
+                        LargeImageText = "www.skidforum.xyz",
+                        SmallImageText = "https://discord.gg/4nuq8fx",
+                        SmallImageKey = "main"
+                    }
+                }); ;
+            }
+            outputbox.Text = "";
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            initalized = true;
+            client = new DiscordRpcClient("765685233176805386");
+            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+            client.Initialize();
+
+            if (initalized == false)
+            {
+                MessageBox.Show("You need to initialize app first!");
+            }
+            else
+            {
+                client.SetPresence(new DiscordRPC.RichPresence()
+                {
+                    Details = "Saving some important shit",
+                    State = "www.skidforum.xyz",
+                    Timestamps = Timestamps.Now,
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = "main",
+                        LargeImageText = "www.skidforum.xyz",
+                        SmallImageText = "https://discord.gg/4nuq8fx",
+                        SmallImageKey = "main"
+                    }
+                }); ;
+            }
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Text Documents(*.txt)|*.txt|All Files(*.*)|*.*";
+                saveFileDialog.Title = "Save Results";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    this.outputbox.SaveFile(saveFileDialog.FileName, RichTextBoxStreamType.PlainText);
+                this.Text = saveFileDialog.FileName;
+
+            }
+            catch (Exception)
+            {
+                int num = (int)MessageBox.Show("Something went wrong :/", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+
+
+        }
+
+        private void metroButton4_Click(object sender, EventArgs e)
+        {
+
+            initalized = true;
+            client = new DiscordRpcClient("765685233176805386");
+            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+            client.Initialize();
+
+            if (initalized == false)
+            {
+                MessageBox.Show("You need to initialize app first!");
+            }
+            else
+            {
+                client.SetPresence(new DiscordRPC.RichPresence()
+                {
+                    Details = "PortScanning some skids.",
+                    State = "www.skidforum.xyz",
+                    Timestamps = Timestamps.Now,
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = "main",
+                        LargeImageText = "www.skidforum.xyz",
+                        SmallImageText = "https://discord.gg/4nuq8fx",
+                        SmallImageKey = "main"
+                    }
+                }); ;
+            }
+            outputbox.Text = this.wb.DownloadString(string.Format("https://api.hackertarget.com/nmap/?q=" + this.IPAddress.Text));
+        }
+
+        private void metroButton5_Click(object sender, EventArgs e)
+        {
+            initalized = true;
+            client = new DiscordRpcClient("765685233176805386");
+            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+            client.Initialize();
+
+            if (initalized == false)
+            {
+                MessageBox.Show("You need to initialize app first!");
+            }
+            else
+            {
+                client.SetPresence(new DiscordRPC.RichPresence()
+                {
+                    Details = "Pinging skids",
+                    State = "www.skidforum.xyz",
+                    Timestamps = Timestamps.Now,
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = "main",
+                        LargeImageText = "www.skidforum.xyz",
+                        SmallImageText = "https://discord.gg/4nuq8fx",
+                        SmallImageKey = "main"
+                    }
+                }); ;
+            }
+            outputbox.Text = this.wb.DownloadString(string.Format("https://api.hackertarget.com/nping/?q=" + this.IPAddress.Text));
+        }
+
+        private void metroButton6_Click(object sender, EventArgs e)
+        {
+            initalized = true;
+            client = new DiscordRpcClient("765685233176805386");
+            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+            client.Initialize();
+
+            if (initalized == false)
+            {
+                MessageBox.Show("You need to initialize app first!");
+            }
+            else
+            {
+                client.SetPresence(new DiscordRPC.RichPresence()
+                {
+                    Details = "Revese Ip Lookup",
+                    State = "www.skidforum.xyz",
+                    Timestamps = Timestamps.Now,
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = "main",
+                        LargeImageText = "www.skidforum.xyz",
+                        SmallImageText = "https://discord.gg/4nuq8fx",
+                        SmallImageKey = "main"
+                    }
+                }); ;
+            }
+            outputbox.Text = this.wb.DownloadString(string.Format("https://api.hackertarget.com/reverseiplookup/?q=" + this.IPAddress.Text));
+        }
+    }
+}
